a85d907400e2f0315d514ac621b0e8e48b6bf00a
Jan Baudisch <jan.baudisch@uni-ulm.de>
2024-11-15 09:07:33 +0100

    fix(cryptominisat): add cadical dependency

--- d4-cryptominisat-sys/build.rs
+++ d4-cryptominisat-sys/build.rs
@@ -5,13 +5,22 @@ fn main() {
         .define("BUILD_SHARED_LIBS", "OFF")
         .define("BINARY", "OFF")
         .define("CMAKE_PREFIX_PATH", env::var("DEP_CADIBACK_ROOT").unwrap())
+        .define("CMAKE_PREFIX_PATH", env::var("DEP_CADICAL_ROOT").unwrap())
         .define(
             "CMAKE_INCLUDE_PATH",
             env::var("DEP_CADIBACK_INCLUDE").unwrap(),
         )
+        .define(
+            "CMAKE_INCLUDE_PATH",
+            env::var("DEP_CADICAL_INCLUDE").unwrap(),
+        )
         .cxxflag(format!(
             "-isystem {}",
             env::var("DEP_CADIBACK_INCLUDE").unwrap()
+        ))
+        .cxxflag(format!(
+            "-isystem {}",
+            env::var("DEP_CADICAL_INCLUDE").unwrap()
         ))
         .build();
 
